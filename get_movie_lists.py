185f96283771e137840cd7de3e7b6d200cc2e92f
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-13 16:40:28 +0900

    get channel snippet

--- get_movie_lists.py
+++ get_movie_lists.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-
 from authentication import build_youtube_service
+import json
 
 
 def main() -> None:
-    pass
+    youtube = build_youtube_service()
+    target_channel_id = 'UCLhUvJ_wO9hOvv_yYENu4fQ'
+    get_channel_request = youtube.channels().list(part='snippet', id=target_channel_id)
+    channel_data = get_channel_request.execute()
+    print(json.dumps(channel_data, indent=4))
 
 
 if __name__ == '__main__':
