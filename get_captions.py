e5fcff9904e3cdfc818d3d9a07edbb1fc726d202
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-13 19:42:42 +0900

    add videoInfo

--- get_captions.py
+++ get_captions.py
@@ -3,7 +3,13 @@
 from apiclient.discovery import Resource
 from authentication import build_youtube_service
 from typing import List, Mapping, Sequence, NamedTuple, Optional
+from pathlib import Path
 import datetime
+
+
+class VideoInfo(NamedTuple):
+    video_id: str,
+    title: str
 
 
 class CaptionInfo(NamedTuple):
@@ -57,6 +63,21 @@ def get_video_ids_from_playlist_id(
     return get_list_result_with_fields(collection, filters, field_selectors)
 
 
+def get_video_info_from_video_id(
+        service: Resource,
+        target_video_id: str
+) -> VideoInfo:
+    collection = service.videos()
+    filters = dict(id=target_video_id)
+    parts = ['snippet']
+    part = ','.join(parts)
+    fields = 'items(snippet(title))'
+    request = collection.list(part=part, fields=fields, **filters)
+    response = request.execute()['items'][0]
+    video_info = VideoInfo(target_video_id, response['snippet']['title'])
+    return video_info
+
+
 def iso_8601_string_to_time(string: str) -> datetime.datetime:
     accepts_format = 'YYYY-MM-DDThh:mm:ss'
     time_format = '%Y-%m-%dT%H:%M:%S'
@@ -73,15 +94,12 @@ def check_caption(
     # last update time check
     caption_updated_time = iso_8601_string_to_time(caption_info.last_updated)
     if caption_updated_time <= last_updated:
-        print('caption not updated!')
         return False
     # language check
     if caption_info.language[:2] != 'ja':
-        print('caption is written in {}!'.format(caption_info.language))
         return False
     # auto generated check
     if caption_info.track_kind == 'ASR':
-        print('caption is ASR!')
         return False
     return True
 
@@ -116,11 +134,17 @@ def main() -> None:
     youtube = build_youtube_service()
     target_channel_id = 'UCLhUvJ_wO9hOvv_yYENu4fQ'
     playlist_id = get_playlist_id_from_channel_id(youtube, target_channel_id)
-    video_ids = get_video_ids_from_playlist_id(youtube, playlist_id, num_max_results=30)
+    video_ids = get_video_ids_from_playlist_id(youtube, playlist_id)
+
+    output_dir = Path('__file__').resolve().parent / 'output'
+    output_dir.mkdir(exist_ok=True)
+
     caption_infos = dict()
     for video_id in video_ids:
         caption_info = get_caption_info_from_video_id(youtube, video_id, datetime.datetime.min)
         if caption_info is not None:
+            video_info = get_video_info_from_video_id(youtube, video_id)
+            # TODO: use video info
             caption_infos[video_id] = caption_info
     print(caption_infos)
 
