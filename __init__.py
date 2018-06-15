b2ff0c54b28cfad7886bb25e2884b5716feb9edf
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-15 17:35:35 +0900

    rename and some refactoring

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .main import main as get_caption_data
+from .youtube_api import CaptionInfo, VideoInfo, Caption
--- youtube_api/__init__.py
+++ youtube_api/__init__.py
@@ -1,4 +1,5 @@
 from .caption_info import CaptionInfo
 from .video_info import VideoInfo
+from .caption import Caption
 from .youtube_api import YoutubeAPI
 from .dirty_youtube_api import DirtyYoutubeAPI
