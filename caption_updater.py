01f2724d8658fffcee4fb1b2b9ebf0d6cb585efe
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2018-06-15 11:30:04 +0900

    add logs

--- caption_updater.py
+++ caption_updater.py
@@ -34,13 +34,19 @@ class CaptionUpdater:
         # last update time check
         caption_updated_time = self._iso_8601_string_to_time(caption_info.last_updated)
         if caption_updated_time <= last_updated:
+            self._logger.debug('Last update for this caption is {} <= memorized last update is {}'
+                               .format(caption_updated_time, last_updated))
             return False
         # language check
         if caption_info.language[:2] != 'ja':
+            self._logger.debug('caption is written in {}, not {}'
+                               .format(caption_info.language, 'ja'))
             return False
         # auto generated check
         if caption_info.track_kind == 'ASR':
+            self._logger.debug('caption is automatically generated')
             return False
+        self._logger.info('caption {} is matched.'.format(caption_info))
         return True
 
     def _get_valid_caption(
@@ -71,6 +77,7 @@ class CaptionUpdater:
             caption_infos = self._youtube_api.get_caption_infos_from_video_id(video_id)
             caption_info = self._get_valid_caption(caption_infos, datetime.datetime.min)
             if caption_info is not None:
+                self._logger.debug('valid caption is found. downloading.')
                 caption = self._youtube_api.download_caption(caption_info.caption_id)
                 video_info = self._youtube_api.get_video_info_from_video_id(video_id)
                 print(caption_info, video_info, caption)
